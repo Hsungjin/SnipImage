8b859757e4440f12c69606de280958c5a3abdd98
Hsungjin <hsungjin142@gmail.com>
2024-03-09 03:59:26 +0900

    [Add] CheckPermision of Photo and Photo

--- Sources/SnipImage/SnipImage.swift
+++ Sources/SnipImage/SnipImage.swift
@@ -1,14 +1,33 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import AVFoundation
+import PhotosUI
 
-public struct testLibrary {
-    public var text = "Hello sungjin"
+public class checkPermission {
     
-    public func test() {
-        print("hihihihi")
+    public static func authorizeCamera(completion: @escaping (Bool) -> Void) {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            completion(true)
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                completion(granted)
+            }
+        default:
+            completion(false)
+        }
     }
     
-    public init() {
-        
+    public static func authorizePhotoLibrary(completion: @escaping (Bool) -> Void) {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized:
+            completion(true)
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization { status in
+                completion(status == .authorized)
+            }
+        default:
+            completion(false)
+        }
     }
 }
